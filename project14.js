52ee8146aedd82e6bb231f336664948bab1ee4a5
hoppa12 <troutwaterworld@gmail.com>
2017-11-22 11:13:29 +0000

    Create project14.js

---
+++ project14.js
@@ -0,0 +1,29 @@
+const nextNum =  num => num % 2 ===0 ? num / 2 : (num * 3)+ 1
+
+const hailStone = count => {
+let res = 1
+    while(count != 1){
+
+            count = nextNum(count)
+            res++
+    }
+
+return res
+
+}
+
+console.time("test")
+let total = {num:0,sequence:0}
+
+for( var x = 1; x < 1000000;x++){
+
+    let res = hailStone(x)
+   total = total.sequence < res ? {num:x,sequence:res} : total
+
+
+}
+
+
+console.log(total.num)
+console.timeEnd("test")
+
